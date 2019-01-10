ec88becd44d1e4aa5873e93a7070a2a9770e110d
zhakui <zh-ge@163.com>
2019-01-10 18:17:12 +0800

    Add new funcation

--- ulex-framework/src/main/java/com/zhkui/ulex/protocol/RPC/AbstractProvider.java
+++ ulex-framework/src/main/java/com/zhkui/ulex/protocol/RPC/AbstractProvider.java
@@ -3,7 +3,17 @@ package com.zhkui.ulex.protocol.RPC;
 import com.zhkui.ulex.common.Request;
 import com.zhkui.ulex.common.Response;
 
+import java.lang.reflect.Method;
+
 public abstract class AbstractProvider<T> implements Provider<T>{
+
+    private Class<T> clz;
+    private T proxyImpl;
+
+    public AbstractProvider(T proxyImpl, Class<T> clz){
+        this.clz = clz;
+        this.proxyImpl = proxyImpl;
+    }
 
 
     public Response call(Request request){
@@ -11,4 +21,27 @@ public abstract class AbstractProvider<T> implements Provider<T>{
     }
 
     protected abstract Response invoke(Request request);
+
+    /**
+     * @return Class<T> return the clz
+     */
+    public Class<T> getInterface() {
+        return clz;
+    }
+
+    public Method lookup(Request request){
+        try{
+            return clz.getMethod(request.getMethodName(),request.getParameterTypes());
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * @return T return the proxyImpl
+     */
+    public T getProxyImpl() {
+        return proxyImpl;
+    }
 }
