96e702616b881cb4aa469d1a8e0c593c10f954a1
zhakui <zh-ge@163.com>
2019-02-13 15:39:37 +0800

    Modify the code`

--- ulex-framework/src/main/java/com/zhkui/ulex/registry/RegistryEntity.java
+++ ulex-framework/src/main/java/com/zhkui/ulex/registry/RegistryEntity.java
@@ -19,4 +19,75 @@ public class RegistryEntity implements Serializable {
         this.username = username;
         this.password = password;
     }
+
+    /**
+     * @return String return the host
+     */
+    public String getHost() {
+        return host;
+    }
+
+    /**
+     * @param host the host to set
+     */
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    /**
+     * @return int return the port
+     */
+    public int getPort() {
+        return port;
+    }
+
+    /**
+     * @param port the port to set
+     */
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    /**
+     * @return String return the path
+     */
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * @param path the path to set
+     */
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    /**
+     * @return String return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return String return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
